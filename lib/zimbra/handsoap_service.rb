1fd89c0464c7ec3cfee42660b9449bbd804df475
Matt Mencel <mr-mencel@wiu.edu>
2015-08-28 13:35:19 -0500

    searchfolder
    searchfolder
    
    searchfolder
    
    searchfolder
    
    updates
    
    testing
    
    handsoapaccountservice
    
    redesign
    
    add attrs
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder
    
    create searchfolder

--- lib/zimbra/handsoap_service.rb
+++ lib/zimbra/handsoap_service.rb
@@ -14,6 +14,7 @@ module Zimbra
       report_error(response) if http_error?
     end
     def report_error(response)
+      puts response
       message = response.body.scan(/<faultstring>(.*)<\/faultstring>/).first.first
       raise SOAPFault, message
     end
