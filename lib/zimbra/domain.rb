77294fa44d2429bafb21cf8f2ec4f6acea2f5fd3
Patricio Bruna <pbruna@gmail.com>
2016-02-10 16:34:54 -0300

    Changes domains

--- lib/zimbra/domain.rb
+++ lib/zimbra/domain.rb
@@ -62,13 +62,15 @@ module Zimbra
 
         def set_max_accounts(message, id, max_accounts, cos_max_accounts)
           message.add 'id', id
-          message.add 'a', max_accounts do |c|
-            c.set_attr 'n', 'zimbraDomainMaxAccounts'
-          end
+          A.inject(message, 'zimbraDomainMaxAccounts', max_accounts)
+          # message.add 'a', max_accounts do |c|
+          #   c.set_attr 'n', 'zimbraDomainMaxAccounts'
+          # end
           cos_max_accounts.each do |cos|
-            message.add 'a', cos do |c|
-              c.set_attr 'n', 'zimbraDomainCOSMaxAccounts'
-            end
+            A.inject(message, 'zimbraDomainCOSMaxAccounts', cos)
+            # message.add 'a', cos do |c|
+            #   c.set_attr 'n', 'zimbraDomainCOSMaxAccounts'
+            # end
           end
         end
 
